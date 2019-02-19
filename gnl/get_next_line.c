0ed826a2f33c894a016324ad45aca6e6330d3204
Gerard Quence <gquence@e3r2p14.21-school.ru>
2019-02-19 18:49:25 +0300

    1

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gquence <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 16:50:58 by gquence           #+#    #+#             */
-/*   Updated: 2019/02/19 16:58:18 by gquence          ###   ########.fr       */
+/*   Updated: 2019/02/19 18:48:58 by gquence          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,34 @@
 #include <string.h>
 #include <stdlib.h>
 
+static	char	*ft_get_line()
+
+static	t_list	*ft_corr_file(t_list **file, const int fd)
+{
+	t_list		*lst;
+
+	if (fd < 0 || !file || !*file)
+		return (NULL);
+	lst = *file;
+	while (lst->next)
+	{
+		if (lst->content_size == fd)
+			return (lst);
+		lst = lst->next;
+	}
+	if (!(lst = ft_lstnew("", fd)))
+		return (NULL);
+	return (lst);
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	static t_list	*file;
-	t_list			*cur;
+	t_list			*curr;
 	char			*content;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || (BUF_SIZE < 1))
 		return (-1);
-	
+	if (!(curr = ft_corr_file(&curr, fd)))
+		return (-1);
 }
