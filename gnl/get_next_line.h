4fd1a15940b4f40bb82d12852b1e5e240137a152
Gerard Quence <gquence@e3r2p12.21-school.ru>
2019-03-22 15:41:43 +0300

    125/100. all tests passed.

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: gquence <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 15:57:12 by gquence           #+#    #+#             */
-/*   Updated: 2019/03/03 16:15:43 by gquence          ###   ########.fr       */
+/*   Updated: 2019/03/22 15:39:56 by gquence          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/includes/libft.h"
+# include "libft.h"
 
 int		get_next_line(const int fd, char **line);
 
-# define BUFF_SIZE  32
+# define BUFF_SIZE  500
 
 #endif
